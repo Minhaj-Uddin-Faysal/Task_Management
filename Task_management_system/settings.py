a1eeea45d76bc6f650bc2e68a5ec3b470bba88da
Minhaj-Uddin-Faysal <minhaj5468@gmail.com>
2025-07-05 10:11:15 +0600

    Added file for deployment

--- Task_management_system/settings.py
+++ Task_management_system/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,3 +128,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+ALLOWED_HOSTS = ['Task_Management_System.onrender.com']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
